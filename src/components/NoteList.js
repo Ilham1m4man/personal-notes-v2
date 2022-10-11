cfd9aa7e7b8c313b2d317ee37e6027f416e1f965
Ilham1m4man <kucingdiki@gmail.com>
2022-10-11 20:25:25 +0700

    Add component to place notes & message if no notes

---
+++ src/components/NoteList.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import NoteItem from './NoteItem';
+import PropTypes from 'prop-types';
+
+function NoteList({ notes }) {
+    if (notes.length === 0) {
+        return (
+            <section className="notes-list-empty">
+                <p className="notes-list__empty">There is no note</p>
+            </section>
+        )
+    } else {
+        return (
+            <section className="notes-list">
+                {
+                    notes.map((note) => (
+                        <NoteItem
+                            key={note.id}
+                            id={note.id}
+                            {...note} />
+                    ))
+                }
+            </section>
+        )
+    }
+}
+
+NoteList.propTypes = {
+    notes: PropTypes.arrayOf(PropTypes.object).isRequired,
+}
+
+export default NoteList;
