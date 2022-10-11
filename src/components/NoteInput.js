114f8cdccac5609104a55bd95d5c9d2ad0e83915
Ilham1m4man <kucingdiki@gmail.com>
2022-10-11 20:22:17 +0700

    Add component to provide user to input their notes

---
+++ src/components/NoteInput.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SaveButton from '../components/buttons/SaveButton';
+
+class NoteInput extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            title: '',
+            body: '',
+        }
+
+        this.onTitleChangeEventHandler = this.onTitleChangeEventHandler.bind(this);
+        this.onBodyChangeEventHandler = this.onBodyChangeEventHandler.bind(this);
+        this.onSubmitEventHandler = this.onSubmitEventHandler.bind(this);
+    }
+
+    onTitleChangeEventHandler(event) {
+        this.setState(() => {
+            return {
+                title: event.target.value,
+            }
+        });
+    }
+
+    onBodyChangeEventHandler(event) {
+        this.setState(() => {
+            return {
+                body: event.target.value,
+            }
+        });
+    }
+
+    onSubmitEventHandler(event) {
+        event.preventDefault();
+        this.props.addNote(this.state);
+    }
+
+    render() {
+        return (
+            <form className='add-new-page__input' onSubmit={this.onSubmitEventHandler}>
+                <input className='add-new-page__input__title' type="text" placeholder="Title" value={this.state.title} onChange={this.onTitleChangeEventHandler} />
+                <textarea className='add-new-page__input__body' type="text" placeholder="Description" value={this.state.body} onChange={this.onBodyChangeEventHandler} />
+                <SaveButton />
+            </form>
+        )
+    }
+}
+
+NoteInput.propTypes = {
+    addNote: PropTypes.func.isRequired
+}
+
+export default NoteInput;
