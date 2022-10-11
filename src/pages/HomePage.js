9ca4f28c42c873316185d0a96fc53cc19d8fddb5
Ilham1m4man <kucingdiki@gmail.com>
2022-10-11 20:32:22 +0700

    Add page to display list of note & create note btn

---
+++ src/pages/HomePage.js
@@ -0,0 +1,58 @@
+import React from 'react';
+import { useSearchParams } from 'react-router-dom';
+import NoteList from '../components/NoteList';
+import SearchBar from '../components/SearchBar';
+import { getActiveNotes } from '../utils/local-data';
+import CreateButton from '../components/buttons/CreateNoteButton';
+
+function HomePageWrapper() {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const keyword = searchParams.get('keyword');
+    function changeSearchParams(keyword) {
+        setSearchParams({ keyword });
+    }
+
+    return <HomePage defaultKeyword={keyword} keywordChange={changeSearchParams} />
+}
+
+class HomePage extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            notes: getActiveNotes(),
+            keyword: props.defaultKeyword || '',
+        }
+
+        this.onKeywordChangeHandler = this.onKeywordChangeHandler.bind(this);
+    }
+
+    onKeywordChangeHandler(keyword) {
+        this.setState(() => {
+            return {
+                keyword,
+            }
+        });
+
+        this.props.keywordChange(keyword);
+    }
+
+    render() {
+        const notes = this.state.notes.filter((note) => {
+            return note.title.toLowerCase().includes(
+                this.state.keyword.toLowerCase()
+            );
+        });
+
+        return (
+            <section>
+                <h2>Your Notes</h2>
+                <SearchBar keyword={this.state.keyword} keywordChange={this.onKeywordChangeHandler} />
+                <NoteList notes={notes} />
+                <CreateButton />
+            </section>
+        )
+    }
+}
+
+export default HomePageWrapper;
