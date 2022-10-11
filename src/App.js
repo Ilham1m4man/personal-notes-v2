51b96f4db4fea963309aa36b9fe9731d18f5a127
Ilham1m4man <kucingdiki@gmail.com>
2022-10-11 20:18:21 +0700

    Render page as per URL

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Route, Routes, Link } from 'react-router-dom';
+import CreateNotePage from './pages/CreateNotePage';
+import HomePage from './pages/HomePage';
+import ArchivePage from './pages/ArchivePage';
+import DetailPage from './pages/DetailPage';
+import Navigation from './components/Navigation';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app-container">
+      <header>
+        <h1><Link to="/">Personal Note</Link></h1>
+        <Navigation />
       </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/create" element={<CreateNotePage />} />
+          <Route path="/detail/:id" element={<DetailPage />} />
+          <Route path="/archive" element={<ArchivePage />} />
+        </Routes>
+      </main>
     </div>
   );
 }
