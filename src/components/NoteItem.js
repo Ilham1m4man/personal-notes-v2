27a933985c2adf72dc3b75111e233d65d4e170f5
Ilham1m4man <kucingdiki@gmail.com>
2022-10-11 20:23:08 +0700

    Add component to render all notes that available

---
+++ src/components/NoteItem.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { showFormattedDate } from '../utils';
+import { Link } from 'react-router-dom';
+
+function NoteItem({ id, title, body, createdAt }) {
+    return (
+        <div className="note-item">
+            <h3 className="note-item__title"><Link to={{pathname: `/detail/${id}`}}>{title}</Link></h3>
+            <p className="note-item__createdAt">{showFormattedDate(createdAt)}</p>
+            <p className="note-item__body">{body}</p>
+        </div>
+    );
+}
+
+NoteItem.propTypes = {
+    title: PropTypes.string.isRequired,
+    body: PropTypes.string.isRequired,
+    createdAt: PropTypes.string.isRequired,
+}
+
+export default NoteItem;
